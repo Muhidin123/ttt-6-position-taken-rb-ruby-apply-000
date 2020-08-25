b757f5dc9be4c2ef3d96d1756f3b6f9050581981
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-25 16:56:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, ix)
   if board[ix] == " " or board[ix] == "" || nil
-  return false
-  ##return true
+    return false
+  else board[ix] == "X" || board[ix] == "O"
+    return true
   end
 end
